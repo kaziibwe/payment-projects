b2104e38e78b825fe7017fab1a6de079c1c6c5a5
alfredkaziibwe19@gmail.com <alfredkaziibwe19@gmail.com>
2024-01-18 03:42:16 -0500

    changed

--- payLoans.php
+++ payLoans.php
@@ -1,12 +1,2 @@
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=h, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    helo
-</body>
-</html>
+we are to pay loans
